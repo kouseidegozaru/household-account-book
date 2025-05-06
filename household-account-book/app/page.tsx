fa190598f7d803b8cc42a2e89a31a468314b87ff
kouseidegozaru <gguangchui@gmail.com>
2025-05-06 15:38:42 +0900

    タブ表示用の仮データの作成

--- household-account-book/app/page.tsx
+++ household-account-book/app/page.tsx
@@ -8,6 +8,21 @@ export type Tab = {
   content: JSX.Element | string;
   onActivate?: () => void;
 };
+
+const tabs: Tab[] = [
+  {id: 'tab1', 
+    label: 'タブ1', 
+    content: 'タブ1の内容',
+    onActivate: () => {console.log('タブ1がアクティブになりました');}
+  },
+  { 
+    id: 'tab2', 
+    label: 'タブ2', 
+    content: 'タブ2の内容',
+    onActivate: () => {console.log('タブ2がアクティブになりました');}
+  },
+];
+
   return (
     <main>
       <h1>Home</h1>
