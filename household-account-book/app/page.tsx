2be1753e3d642ad570a8e1f29520730bc802959c
kouseidegozaru <gguangchui@gmail.com>
2025-05-06 15:37:51 +0900

    タブコンテンツの型を定義

--- household-account-book/app/page.tsx
+++ household-account-book/app/page.tsx
@@ -1,5 +1,13 @@
+'use client';
 
-export default function Home() {
+import { JSX, useState } from 'react';
+
+export type Tab = {
+  id: string;
+  label: string;
+  content: JSX.Element | string;
+  onActivate?: () => void;
+};
   return (
     <main>
       <h1>Home</h1>
