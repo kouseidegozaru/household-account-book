d060cac37def15960a3d6f549c29b429f1196f64
kouseidegozaru <gguangchui@gmail.com>
2025-05-08 15:30:33 +0900

    図の作成

--- household-account-book/app/monthly/content.tsx
+++ household-account-book/app/monthly/content.tsx
@@ -1,10 +1,15 @@
 'use client'
 
 import { useState, useEffect } from "react"
+import { Chart, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 import { DisplayField } from "../_calculate/devide";
 import { Cycle } from "../_storage/handler";
 import { useStorage } from "../_storage/handler"
 import asMonthly from "../_calculate/asMonthly"
+
+Chart.register(ArcElement, Tooltip, Legend, ChartDataLabels);
 
 export default function MonthlyContent() {
     const [displayTable, setDisplayTable] = useState<DisplayField[]>([])
@@ -18,6 +23,15 @@ export default function MonthlyContent() {
         setDisplayTable(displayTable)
 
     }, [tableData])
+
+    // 日毎
+    const getDailyItems = (displayTable : DisplayField[]) => displayTable.filter((record) => record.cycle === Cycle.Daily)
+    // 週毎
+    const getWeeklyItems = (displayTable : DisplayField[]) => displayTable.filter((record) => record.cycle === Cycle.Weekly)
+    // 月毎
+    const getMonthlyItems = (displayTable : DisplayField[]) => displayTable.filter((record) => record.cycle === Cycle.Monthly)
+    // 年毎
+    const getYearlyItems = (displayTable : DisplayField[]) => displayTable.filter((record) => record.cycle === Cycle.Yearly)
 
     // 支出として扱うもの
     const getExpensesItems = (displayTable : DisplayField[]) => displayTable.filter((record) => record.cycle !== Cycle.Yearly)
@@ -41,6 +55,43 @@ export default function MonthlyContent() {
                 <p className="text-3xl font-bold">¥{amountTotal(displayTable).toLocaleString()}</p>
                 <p className="text-sm mb-4 mt-1 text-text-primary">{new Date().getFullYear()}年 {new Date().getMonth() + 1}月</p>
             </div>
+
+            <div className="mx-9 mt-4 mb-4">
+                <Pie
+                    data={{
+                        labels: ['日毎', '週毎', '月毎', '年毎'],
+                        datasets: [
+                            {
+                                label: 'Cycle',
+                                data: [
+                                    amountTotal(getDailyItems(displayTable)),
+                                    amountTotal(getWeeklyItems(displayTable)),
+                                    amountTotal(getMonthlyItems(displayTable)),
+                                    amountTotal(getYearlyItems(displayTable))
+                                ],
+                                backgroundColor: ['#004F46', '#047C72', '#4BACA0', '#7FDED1'],
+                            },
+                        ],
+                    }}
+                    options={{
+                        responsive: true,
+                        plugins: {
+                            legend: {
+                                position: 'top',
+                            },
+                            title: {
+                                display: true,
+                                text: 'Cycle',
+                            },
+                            datalabels: {
+                                color: '#fff',
+                                formatter: (value, context) => { return '¥' + value.toLocaleString() },
+                            },
+                        },
+                    }}
+                />
+            </div>
+
             <table>
                 <thead>
                     <tr>
